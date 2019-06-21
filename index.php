cc632d811da8225eed82239f4dffa9fc36016b2f
krittiyaclark <krittiyaclark@gmail.com>
2019-06-21 00:41:44 -0400

    Move posts_meta and readmore_link to the helpers.php file

--- index.php
+++ index.php
@@ -7,19 +7,12 @@
             <a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
         </h2>
         <div>
-            Posted on
-            <a href="<?php echo get_permalink( ) ?>">
-                <time datetime="<?php echo get_the_date('c') ?>"><?php echo get_the_date(); ?></time>
-            </a>
-            By <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ) ?>"><?php echo
-            get_the_author( ); ?></a>
+            <?php customtheme_posts_meta(); ?>
         </div>
         <div>
             <?php the_excerpt(); ?>
         </div>
-        <a href="<?php echo get_permalink( ) ?>" title="<?php the_title_attribute(); ?>">
-            Read More <span class="u-screen-reader-text">About <?php the_title() ?></span>
-        </a>
+            <?php customtheme_readmore_link(); ?>
         <?php } ?>
         <?php the_posts_pagination(); ?>
 <?php } else { ?> 
