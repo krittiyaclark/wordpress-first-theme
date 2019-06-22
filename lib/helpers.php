b9ab697d6576c56d70282a30d7bfd074ed3e19a2
krittiyaclark <krittiyaclark@gmail.com>
2019-06-22 01:29:26 -0400

    Internationalization & Localization

--- lib/helpers.php
+++ lib/helpers.php
@@ -1,17 +1,33 @@
 <?php
 
 function customtheme_posts_meta() {
-    echo 'Posted on ';
-    echo '<a href="' . esc_url(get_permalink( )) . '">';
-    echo '<time datetime="' . esc_attr(get_the_date('c')) . '">' . get_the_date() . '</time>';
-    echo '</a>';
-    echo 'By <a href="' . esc_url(get_author_posts_url( get_the_author_meta( 'ID' ) )) . '">' . esc_html(get_the_author( )) . 
-    '</a>';
+    /*  translator:: %s Post Date */
+    printf(
+        esc_html__( 'Posted on %s', '_customtheme'),
+        '<a href="' . esc_url(get_permalink( )) . '"><time datetime="' . esc_attr(get_the_date('c')) . '">' . get_the_date() . '</time></a>'
+    );
+    /* translator: %s: Post Author*/
+    printf(
+         esc_html__(' By %s', '_customtheme'),
+         '<a href="' . esc_url(get_author_posts_url( get_the_author_meta( 'ID' ) )) . '">' . esc_html(get_the_author( )) . 
+         '</a>'
+    );
 }
 
 function customtheme_readmore_link() {
     echo '<a href="' . esc_url(get_permalink()) . '" title="' . the_title_attribute(['echo' => false]) . '">';
-    echo 'Read More <span class="u-screen-reader-text">About ' . the_title() . '</span>';
+    /* tranlators: %s: Post Title */
+    printf(
+        wp_kses(
+            __( 'Read More <span class="u-screen-reader-text">About %s</span>', '_customtheme' ),
+            [
+                    'span' => [
+                        'class' => []
+                    ]
+            ]
+        ),
+        get_the_title()
+    );
     echo '</a>';
 }
 
