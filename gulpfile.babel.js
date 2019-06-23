242c6692df07a333abae14840b107bcc44359333
krittiyaclark <krittiyaclark@gmail.com>
2019-06-22 22:21:11 -0400

    Creating A Gulp Task For Styles

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,12 +3,26 @@ import yargs from 'yargs'
 import sass from 'gulp-sass'
 import cleanCSS from 'gulp-clean-css'
 import gulpif from 'gulp-if'
+import sourcemaps from 'gulp-sourcemaps'
 
 const PRODUCTION = yargs.argv.prod
 
+// refactor
+const paths = {
+    styles: {
+        src: ['src/assets/scss/bundle.scss', 'src/assets/scss/admin.scss'],
+        dest: 'dist/assets/css'
+    }
+}
 export const styles = () => {
-    return gulp.src('src/assets/scss/bundle.scss')
+    return gulp.src(paths.styles.src)
+        .pipe(gulpif(!PRODUCTION, sourcemaps.init()))
         .pipe(sass().on('error', sass.logError))
         .pipe(gulpif(PRODUCTION, cleanCSS({compatibility: 'ie8'})))
-        .pipe(gulp.dest('dist/assets/css'));
+        .pipe(gulpif(!PRODUCTION, sourcemaps.write()))
+        .pipe(gulp.dest(paths.styles.dest));
+}
+
+export const watch = () => {
+    gulp.watch('src/assets/scss/**/*scss', styles)
 }
--- lib/enqueue-assets.php
+++ lib/enqueue-assets.php
@@ -5,3 +5,9 @@
    }
 
    add_action('wp_enqueue_scripts', 'customtheme_assets');
+
+   function customtheme_admin_assets() {
+    wp_enqueue_style('customtheme-admin-stylesheet', get_template_directory_uri() . '/dist/assets/css/admin.css', array(), '1.0.0', 'all' );
+}
+
+add_action('admin_enqueue_scripts', 'customtheme_assets');
