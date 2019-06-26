09ea4f2146ca808dc19b142fd34785f542d5a58f
krittiyaclark <krittiyaclark@gmail.com>
2019-06-25 23:57:28 -0400

    Set up compress zip task

--- dist/assets/js/bundle.js
+++ dist/assets/js/bundle.js
@@ -1,10 +1,1 @@
-// import 'jquery'
-import './components/slider'
-import $ from 'jquery' // Include jQuery
-
-// Include jQuery
-// const $ = jQuery;
-
-$('body').click(() =>
-    alert(true)
-)
+!function(n){var r={};function o(e){if(r[e])return r[e].exports;var t=r[e]={i:e,l:!1,exports:{}};return n[e].call(t.exports,t,t.exports,o),t.l=!0,t.exports}o.m=n,o.c=r,o.d=function(e,t,n){o.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:n})},o.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},o.t=function(t,e){if(1&e&&(t=o(t)),8&e)return t;if(4&e&&"object"==typeof t&&t&&t.__esModule)return t;var n=Object.create(null);if(o.r(n),Object.defineProperty(n,"default",{enumerable:!0,value:t}),2&e&&"string"!=typeof t)for(var r in t)o.d(n,r,function(e){return t[e]}.bind(null,r));return n},o.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return o.d(t,"a",t),t},o.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},o.p="",o(o.s=1)}([function(e,t){e.exports=jQuery},function(e,t,n){e.exports=n(2)},function(e,t,n){"use strict";n.r(t),n(3);var r=n(0);n.n(r)()("body").click(function(){return alert(!0)})},function(e,t){console.log("Hello Hello")}]);
