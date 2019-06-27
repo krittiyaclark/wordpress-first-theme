81b100adace3319b79574c278413e87a811bb274
krittiyaclark <krittiyaclark@gmail.com>
2019-06-27 15:45:50 -0400

    Add child theme

--- functions.php
+++ functions.php
@@ -3,38 +3,4 @@
 require_once('lib/helpers.php');
 require_once('lib/enqueue-assets.php');
 
-// Creating Custom Action Hooks
-function after_pagination() {
-    echo 'PHP';
-}
-
-function after_pagination2() {
-    echo 'PHP2';
-}
-
-add_action( '_themename_after_pagination', 'after_pagination', 2);
-add_action( '_themename_after_pagination', 'after_pagination2', 1);
-
-// WordPress Core Actions
-add_action( 'pre_get_posts', 'function_to_add', 10, 1);
-
-function function_to_add($query) {
-    if($query->is_main_query()) {
-        $query->set('posts_per_page', 2);
-    }
-}
-
-// Core & Custom Filter Hooks
-function no_post_text($text) {
-    return esc_html__('No Posts', 'test-child');
-}
-
-add_filter( '_themename_no_posts_text', 'no_posts_text' );
-
-function filter_title($title) {
-    return 'Filtered' .$title;
-}
-
-add_filter( 'the_title', 'filter_title' );
-
 ?>
--- lib/helpers.php
+++ lib/helpers.php
@@ -1,19 +1,21 @@
 <?php
 
-function _themename_posts_meta() {
-    /*  translator:: %s Post Date */
-    printf(
-        esc_html__( 'Posted on %s', '_themename'),
-        '<a href="' . esc_url(get_permalink( )) . '"><time datetime="' . esc_attr(get_the_date('c')) . '">' . get_the_date() . '</time></a>'
-    );
-    /* translator: %s: Post Author*/
-    printf(
-         esc_html__(' By %s', '_themename'),
-         '<a href="' . esc_url(get_author_posts_url( get_the_author_meta( 'ID' ) )) . '">' . esc_html(get_the_author( )) . 
-         '</a>'
-    );
+// override child theme
+if(!function_exists('_themename_posts_meta')) {
+    function _themename_posts_meta() {
+        /*  translator:: %s Post Date */
+        printf(
+            esc_html__( 'Posted on %s', '_themename'),
+            '<a href="' . esc_url(get_permalink( )) . '"><time datetime="' . esc_attr(get_the_date('c')) . '">' . get_the_date() . '</time></a>'
+        );
+        /* translator: %s: Post Author*/
+        printf(
+            esc_html__(' By %s', '_themename'),
+            '<a href="' . esc_url(get_author_posts_url( get_the_author_meta( 'ID' ) )) . '">' . esc_html(get_the_author( )) . 
+            '</a>'
+        );
+    }
 }
-
 function _themename_readmore_link() {
     echo '<a href="' . esc_url(get_permalink()) . '" title="' . the_title_attribute(['echo' => false]) . '">';
     /* tranlators: %s: Post Title */
