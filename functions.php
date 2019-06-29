49a4a54ce21ea9ab097f32fff96a42a89ba00b7b
krittiyaclark <krittiyaclark@gmail.com>
2019-06-29 11:03:12 -0400

    Creating Sidebars For Widgets

--- functions.php
+++ functions.php
@@ -2,5 +2,6 @@
 
 require_once('lib/helpers.php');
 require_once('lib/enqueue-assets.php');
+require_once('lib/sidebars.php');
 
 ?>
--- sidebar.php
+++ sidebar.php
@@ -1,1 +1,4 @@
-sidebar
+<aside role="complementary">
+    <!-- add sidebar after add sidebars.php function -->
+    <?php dynamic_sidebar( 'primary-sidebar'); ?>
+</aside>
