9b965164b8dd948d7a5ed95f742f5f184919198e
krittiyaclark <krittiyaclark@gmail.com>
2019-06-30 22:44:49 -0400

    Organizing Your Templates With get_template_part()

---
+++ template-parts/post/content.php
@@ -0,0 +1,13 @@
+<article <?php post_class('c-post u-margin-bottom-20'); ?>>
+    <?php the_post(); ?>
+    <h2 class="c-post__title">
+        <a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+    </h2>
+    <div class="c-post__meta">
+        <?php _themename_posts_meta(); ?>
+    </div>
+    <div class="c-post__excerpt">
+        <?php the_excerpt(); ?>
+    </div>
+        <?php _themename_readmore_link(); ?>
+</article>
