165088e8557db0c1d304101a54ed77323fb85e12
krittiyaclark <krittiyaclark@gmail.com>
2019-07-02 16:21:00 -0400

    Coding The Header & The Search Form

--- header.php
+++ header.php
@@ -9,3 +9,16 @@
         <link rel="stylesheet" href="">
     </head>
     <body <?php body_class(); ?>>
+
+        <header role="banner" class="u-margin-bottom-40">
+            <div class="class c-header">
+                <div class="o-container u-flex u-align-justify u-align-middle">
+                    <div class="c-header__logo">
+                            <a class="c-header__blogname" href="<?php echo esc_url(home_url( '/' )); ?>"><?php esc_html(bloginfo( 'name' )); ?></a>
+                    </div>
+                    <?php get_search_form( true ); ?>
+                </div>
+            </div>
+        </header>
+
+        <div id="content">
