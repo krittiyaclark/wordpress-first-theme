934da74123a304940d6dacab9f82f57d5ff3b91d
krittiyaclark <krittiyaclark@gmail.com>
2019-07-20 14:07:03 -0400

    Adding The Markup For Menu Accessibility

--- lib/navigation.php
+++ lib/navigation.php
@@ -7,13 +7,34 @@
 	}
 	add_action( 'init', '_themename_register_menus' );
 
+    function _themename_aria_hasdropdown($atts, $item, $args) {
+        if($args->theme_location == 'main-menu') {
+            if(in_array('menu-item-has-children', $item->classes)) {
+                $atts['aria-haspopup'] = 'true';
+                $atts['aria-expanded'] = 'false';
+            }
+        }
+        return $atts;
+    }
+
+    add_filter( 'nav_menu_link_attributes', '_themename_aria_hasdropdown', 10, 3 );
+
+    function _themename_submenu_button($dir = 'down', $title) {
+        $button = '<button class="menu-button">';
+        $button .= '<span class="u-screen-reader-text menu-button-show">' . sprintf(esc_html__('Show %s submenu', '_themename'), $title) . '</span>';
+        $button .= '<span aria-hidden="true" class="u-screen-reader-text menu-button-hide">' . sprintf(esc_html__('Hide %s submenu', '_themename'), $title) . '</span>';
+        $button .= '<i class="fa fa-angle-' . $dir . '" aria-hidden="true"></i>';
+        $button .= '</button>';
+        return $button;
+    }
+
     function _themename_dropdown_icon($title, $item, $args, $depth) {
-    if($args->theme_location == 'main-menu') {
-        if(in_array('menu-item-has-children', $item->classes)) {
-            if($depth == 0) {
-                $title .= '<i class="fa fa-angle-down" aria-hidden="true"></i>';
-            } else {
-                $title .= '<i class="fa fa-angle-right" aria-hidden="true"></i>';
+        if($args->theme_location == 'main-menu') {
+            if(in_array('menu-item-has-children', $item->classes)) {
+                if($depth == 0) {
+                    $title .= _themename_submenu_button('down', $title);
+                } else {
+                    $title .= _themename_submenu_button('right', $title);
             }
         }
     }
