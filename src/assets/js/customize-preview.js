c5d1013a1c1dca6ee878d47f0a98b1d69fc640e6
krittiyaclark <krittiyaclark@gmail.com>
2019-07-25 11:43:04 -0400

    Adding An Option To Show/Hide Author Info

--- src/assets/js/customize-preview.js
+++ src/assets/js/customize-preview.js
@@ -7,6 +7,16 @@ wp.customize( 'blogname', (value) => {
     } )
 })
 
+wp.customize( '_themename_display_author_info', (value) => {
+    value.bind( (to) => {
+        if(to) {
+            $('.c-post-author').show();
+        } else {
+            $('.c-post-author').hide();
+        }
+    } )
+})
+
 wp.customize( '_themename_accent_colour', (value) => {
     value.bind( (to) => {
         let inline_css = ``;
